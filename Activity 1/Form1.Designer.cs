15fa16e34b6e3148adf5ad6a3212fb5cad683719
euzzle <euzzle@LAPTOP-GCHU2N5O>
2025-01-23 11:18:53 +0800

    Add project files.

---
+++ Activity 1/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Activity_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.bntCalculateRectangle = new System.Windows.Forms.Button();
+            this.btn2CalculateCircle = new System.Windows.Forms.Button();
+            this.btn3CalculateTriangle = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 35);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 32);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Length";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(155, 35);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 32);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(177, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Width";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(275, 35);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 32);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(296, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Radius";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 95);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 30);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(145, 95);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 30);
+            this.textBox5.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Base";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(177, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Height";
+            // 
+            // bntCalculateRectangle
+            // 
+            this.bntCalculateRectangle.Location = new System.Drawing.Point(12, 172);
+            this.bntCalculateRectangle.Name = "bntCalculateRectangle";
+            this.bntCalculateRectangle.Size = new System.Drawing.Size(75, 32);
+            this.bntCalculateRectangle.TabIndex = 10;
+            this.bntCalculateRectangle.Text = "Rectangle";
+            this.bntCalculateRectangle.UseVisualStyleBackColor = true;
+            this.bntCalculateRectangle.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn2CalculateCircle
+            // 
+            this.btn2CalculateCircle.Location = new System.Drawing.Point(93, 172);
+            this.btn2CalculateCircle.Name = "btn2CalculateCircle";
+            this.btn2CalculateCircle.Size = new System.Drawing.Size(75, 32);
+            this.btn2CalculateCircle.TabIndex = 11;
+            this.btn2CalculateCircle.Text = "Circle";
+            this.btn2CalculateCircle.UseVisualStyleBackColor = true;
+            this.btn2CalculateCircle.Click += new System.EventHandler(this.btn2CalculateCircle_Click);
+            // 
+            // btn3CalculateTriangle
+            // 
+            this.btn3CalculateTriangle.Location = new System.Drawing.Point(180, 172);
+            this.btn3CalculateTriangle.Name = "btn3CalculateTriangle";
+            this.btn3CalculateTriangle.Size = new System.Drawing.Size(75, 32);
+            this.btn3CalculateTriangle.TabIndex = 12;
+            this.btn3CalculateTriangle.Text = "Triangle";
+            this.btn3CalculateTriangle.UseVisualStyleBackColor = true;
+            this.btn3CalculateTriangle.Click += new System.EventHandler(this.btn3CalculateTriangle_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(423, 30);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(276, 186);
+            this.listBox1.TabIndex = 13;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(275, 95);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 30);
+            this.textBox6.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(308, 79);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn3CalculateTriangle);
+            this.Controls.Add(this.btn2CalculateCircle);
+            this.Controls.Add(this.bntCalculateRectangle);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button bntCalculateRectangle;
+        private System.Windows.Forms.Button btn2CalculateCircle;
+        private System.Windows.Forms.Button btn3CalculateTriangle;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Activity 1/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private double CalculateArea (double length, double width)
+        {
+            return length * width;
+        }
+
+        private double CalculateArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        private double CalculateArea(double baseLength, double height, bool isTriangle)
+        {
+            if (isTriangle)
+            
+                return 0.5 * baseLength * height;
+                return 0;
+
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out double length) && double.TryParse(textBox2.Text, out double width))
+            { 
+                double area = CalculateArea(length, width);
+                listBox1.Items.Add($"Rectangle: Length = {length}, Width ={width}, Area = {area}");
+                textBox6.Text = $"Most Recent Result{area}";
+            }
+
+            else
+            {
+                MessageBox.Show("Enter the numbers of length and width");
+            }
+        }
+
+        private void btn2CalculateCircle_Click(object sender, EventArgs e)
+        {
+           if (double.TryParse(textBox3.Text, out double radius))
+            {
+                double area = CalculateArea(radius);
+                listBox1.Items.Add($"Circle: Radius = {radius}, Area = {area}");
+                textBox6.Text = $"Most Recent Result{area}";
+            }
+
+            else
+            {
+                MessageBox.Show("Enter the number of radius");
+            }
+        }
+
+        private void btn3CalculateTriangle_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox4.Text, out double baseLength) && double.TryParse(textBox5.Text, out double height))
+            {
+                double area = CalculateArea(baseLength, height, true);
+                listBox1.Items.Add($"Triangle: base = {baseLength}, Height = {height}, Area = {area}");
+                textBox6.Text = $"Most Recent Result";
+            }
+            else
+            {
+                MessageBox.Show("Enter the numbers of base and height");
+            }
+        }
+    }
+}
